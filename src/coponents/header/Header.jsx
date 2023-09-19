4959d1124d5f15dc6172b41960626609c9d280e0
BorisMadiou <borismadiou@gmail.com>
2023-09-19 23:32:06 +0200

    test Header

---
+++ src/coponents/header/Header.jsx
@@ -0,0 +1,14 @@
+
+function Header() {
+  return (
+    <header>
+      <img
+        src="./assets/images/header.jpg"
+        alt="Description de l'image"
+      />
+    </header>
+  );
+}
+
+export default Header;
+
